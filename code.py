34cf4774317b101848150c4a125d3e23fb71c0ca
VXDANSH <78954227+VXDANSH@users.noreply.github.com>
2021-06-01 13:51:18 +0530

    code for using it, remember to cd to right folder

---
+++ code.py
@@ -0,0 +1,11 @@
+import os
+
+print("Remember to cd to wherever your files are, before running this program!!")
+files = os.listdir("/Users/vedansh/Desktop/files")
+
+landmark = input("Landmark for files (will get files before this character):     ")
+
+for file in files:
+    pos_of_landmark = file.find(landmark)
+    account_number = file[:pos_of_landmark]
+    print(account_number)
